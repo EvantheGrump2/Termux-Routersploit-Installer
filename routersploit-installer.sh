934dc5b2d614a0ddeecd56afd9d18564ba14fac7
EvantheGrump2 <165404156+EvantheGrump2@users.noreply.github.com>
2024-07-09 16:06:40 +0000

    Update routersploit-installer.sh

--- routersploit-installer.sh
+++ routersploit-installer.sh
@@ -1,1 +1,9 @@
-pkg update && pkg upgrade -y && pkg install clang python libffi openssl rust wget binutils git libsodium -y && SODIUM_INSTALL=system pip install pynacl && pip install setuptools && git clone https://github.com/threat9/routersploit $PREFIX/share/routersploit && cd $PREFIX/share/routersploit/ && python3 -m pip install -r requirements.txt && touch $PREFIX/bin/rsf && echo "python3 $PREFIX/share/routersploit/rsf.py"  > $PREFIX/bin/rsf && chmod +x $PREFIX/bin/rsf && echo "installed" && cd
+pkg update && pkg upgrade -y &&
+pkg install clang python libffi openssl rust wget binutils git libsodium -y && 
+SODIUM_INSTALL=system pip install pynacl && 
+pip install setuptools && 
+git clone https://github.com/threat9/routersploit $PREFIX/share/routersploit &&
+cd $PREFIX/share/routersploit/ && 
+python3 -m pip install -r requirements.txt && 
+touch $PREFIX/bin/rsf && echo "python3 $PREFIX/share/routersploit/rsf.py"  > $PREFIX/bin/rsf && chmod +x $PREFIX/bin/rsf && 
+echo "installed" && cd
